c52bae79643523764954cb51764e43b2ced69fda
20012017 <moll.stephenson@gmail.com>
2017-01-09 15:32:39 +0000

    Adds rows, columns and diagonals to board

--- src/ttt/board_test.go
+++ src/ttt/board_test.go
@@ -5,20 +5,35 @@ import (
 	"testing"
 )
 
-var board Board = NewBoard()
-
 func TestEmptyBoardHasNineSpaces(t *testing.T) {
-	assert.Equal(t, 9, board.Size())
+	assert.Equal(t, 9, NewBoard().Size())
 }
 
 func TestCanPlaceAMarkOnBoard(t *testing.T) {
-	board = board.PlaceMark(4, "X")
+	board := NewBoard().PlaceMark(4, "X")
 	assert.Equal(t, "X", board.MarkAt(4))
 }
 
 func TestCanPlaceTwoMarksOnBoard(t *testing.T) {
-	board = board.PlaceMark(4, "X")
+	board := NewBoard().PlaceMark(4, "X")
 	board = board.PlaceMark(5, "0")
 	assert.Equal(t, "X", board.MarkAt(4))
 	assert.Equal(t, "0", board.MarkAt(5))
+}
+
+var numberedBoard Board = newMarkedBoard([]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+
+func TestCanReturnRows(t *testing.T) {
+	rows := [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
+	assert.Equal(t, rows, numberedBoard.Rows())
+}
+
+func TestCanReturnColumns(t *testing.T) {
+	columns := [][]string{{"1", "4", "7"}, {"2", "5", "8"}, {"3", "6", "9"}}
+	assert.Equal(t, columns, numberedBoard.Columns())
+}
+
+func TestCanReturnDiagonals(t *testing.T) {
+	diagonals := [][]string{{"1", "5", "9"}, {"3", "5", "7"}}
+	assert.Equal(t, diagonals, numberedBoard.Diagonals())
 }
