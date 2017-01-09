443baf4d5e63c69c3e6bc9b31017ffa08593b87e
20012017 <moll.stephenson@gmail.com>
2017-01-09 18:32:46 +0000

    WinningPositions returns all possible winning positions

--- src/ttt/winning-positions.go
+++ src/ttt/winning-positions.go
@@ -1,33 +1,64 @@
 package ttt
 
+const rowLength, columnWidth, firstCell int = 3, 1, 0
+
 type WinningPositions struct {
 	rows, columns, diagonals [][]string
 }
 
 func NewWinningPositions(cells []string) WinningPositions {
-	return WinningPositions{rows(cells), columns(cells), diagonals(cells)}
+	return WinningPositions{
+		rows(cells),
+		columns(cells),
+		diagonals(cells),
+	}
 }
 
 func rows(cells []string) [][]string {
-	row := getRow
-	return [][]string{row(0, cells), row(3, cells), row(6, cells)}
+	return getRows(firstCell, cells)
 }
 
 func columns(cells []string) [][]string {
-	column := getColumn
-	return [][]string{column(0, cells), column(1, cells), column(2, cells)}
+	return getColumns(firstCell, cells)
 }
 
 func diagonals(cells []string) [][]string {
 	return [][]string{getLeftDiagonal(cells), getRightDiagonal(cells)}
 }
 
-func getRow(rowStart int, cells []string) []string {
-	return getCellRange(cells, rowStart, rowStart+2, 1)
+func (winningPositions WinningPositions) All() [][]string {
+	allPositions := [][]string{}
+	allPositions = getAllPositions(allPositions, winningPositions.rows)
+	allPositions = getAllPositions(allPositions, winningPositions.columns)
+	allPositions = getAllPositions(allPositions, winningPositions.diagonals)
+	return allPositions
 }
 
-func getColumn(columnStart int, cells []string) []string {
-	return getCellRange(cells, columnStart, columnStart+6, 3)
+func getAllPositions(allPositions [][]string, positions [][]string) [][]string {
+	for _, row := range positions {
+		allPositions = append(allPositions, row)
+	}
+	return allPositions
+}
+
+func getRows(rowStart int, cells []string) [][]string {
+	rows := [][]string{}
+	for rowStart < (rowLength * rowLength) {
+		row := getCellRange(cells, rowStart, rowStart+(rowLength-1), columnWidth)
+		rows = append(rows, row)
+		rowStart = rowStart + rowLength
+	}
+	return rows
+}
+
+func getColumns(columnStart int, cells []string) [][]string {
+	columns := [][]string{}
+	for columnStart < rowLength {
+		column := getCellRange(cells, columnStart, columnStart+rowLength*2, rowLength)
+		columns = append(columns, column)
+		columnStart = columnStart + columnWidth
+	}
+	return columns
 }
 
 func getLeftDiagonal(cells []string) []string {
--- src/ttt/winning-positions_test.go
+++ src/ttt/winning-positions_test.go
@@ -5,19 +5,27 @@ import (
 	"testing"
 )
 
-var winningPositions WinningPositions = NewWinningPositions([]string{"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+var numberedBoard []string = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+var winningPositions WinningPositions = NewWinningPositions(numberedBoard)
 
 func TestReturnsRows(t *testing.T) {
-	rows := [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
-	assert.Equal(t, rows, winningPositions.rows)
+	expectedRows := [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}
+	assert.Equal(t, expectedRows, winningPositions.rows)
 }
 
 func TestReturnsColumns(t *testing.T) {
-	columns := [][]string{{"1", "4", "7"}, {"2", "5", "8"}, {"3", "6", "9"}}
-	assert.Equal(t, columns, winningPositions.columns)
+	expectedColumns := [][]string{{"1", "4", "7"}, {"2", "5", "8"}, {"3", "6", "9"}}
+	assert.Equal(t, expectedColumns, winningPositions.columns)
 }
 
 func TestReturnsDiagonals(t *testing.T) {
 	diagonals := [][]string{{"1", "5", "9"}, {"3", "5", "7"}}
 	assert.Equal(t, diagonals, winningPositions.diagonals)
+}
+
+func TestReturnsAllPossibleWinningPositions(t *testing.T) {
+	allWinningPositions := [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"},
+		{"1", "4", "7"}, {"2", "5", "8"}, {"3", "6", "9"},
+		{"1", "5", "9"}, {"3", "5", "7"}}
+	assert.Equal(t, allWinningPositions, winningPositions.All())
 }
