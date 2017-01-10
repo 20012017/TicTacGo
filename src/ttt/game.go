7be093a06ce06e49c7a7feefd53225d224ec6676
20012017 <moll.stephenson@gmail.com>
2017-01-10 19:42:50 +0000

    Wraps game field access in game methods

--- src/ttt/game.go
+++ src/ttt/game.go
@@ -23,28 +23,34 @@ func (game Game) currentMark() string {
 }
 
 func (game Game) isOver() bool {
-	board, markOne, markTwo := game.board, game.markOne(), game.markTwo()
-	over := game.rules.isAWin(board, markOne, markTwo) ||
-		game.rules.isADraw(board, markOne, markTwo)
+	over := game.isAWin(game.data()) ||
+		game.rules.isADraw(game.data())
 	return over
 }
 
 func (game Game) result() (bool, string) {
-	if game.isOver() && game.rules.isAWin(game.board, game.markOne(), game.markTwo()) {
+	if game.isOver() && game.isAWin(game.data()) {
 		return true, game.winner()
 	}
 	return false, ""
 }
 
 func (game Game) winner() string {
-	board, markOne, markTwo := game.board, game.markOne(), game.markTwo()
-	return game.rules.winner(board, markOne, markTwo)
+	return game.rules.winner(game.data())
 }
 
 func (game Game) playMove(move int) Board {
 	board := game.board
 	markedBoard := board.placeMark(move, game.currentMark())
 	return markedBoard
+}
+
+func (game Game) isAWin(board Board, markOne, markTwo string) bool {
+	return game.rules.isAWin(board, markOne, markTwo)
+}
+
+func (game Game) data() (Board, string, string) {
+	return game.board, game.markOne(), game.markTwo()
 }
 
 func (game Game) markOne() string {
