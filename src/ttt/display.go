a9fcb7d02dc66be3b5b2fc9f20e47ad6a29a3b75
20012017 <moll.stephenson@gmail.com>
2017-01-11 08:58:46 +0000

    Adds display interface and cli display

--- src/ttt/display.go
+++ src/ttt/display.go
@@ -1,29 +1,11 @@
 package ttt
 
-import "io"
+type Display interface {
+	write(message string)
 
-type Display struct {
-	writer io.Writer
-	script *Script
-}
+	welcome()
 
-func NewDisplay(writer io.Writer, script *Script) Display {
-	return Display{writer, script}
-}
+	prompt()
 
-func (display Display) write(message string) {
-	io.WriteString(display.writer, message)
-}
-
-func (display Display) welcome() {
-	display.write(display.script.welcome())
-}
-
-func (display Display) prompt() {
-	display.write(display.script.prompt())
-}
-
-func (display Display) showBoard(board Board) {
-	displayBoard := new(DisplayBoard).show(board)
-	display.write(displayBoard)
+	showBoard(board Board)
 }
