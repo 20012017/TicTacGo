8da86f1f88c438df273b6928293d81fb346b5554
20012017 <moll.stephenson@gmail.com>
2017-01-11 15:51:28 +0000

    Adds error printing to cligame

--- src/ttt/playerfake.go
+++ src/ttt/playerfake.go
@@ -1,5 +1,7 @@
 package ttt
 
+import "errors"
+
 type PlayerFake struct {
 	mark        string
 	currentMove int
@@ -17,5 +19,12 @@ func (player *PlayerFake) Mark() string {
 func (player *PlayerFake) move(board Board) (int, error) {
 	move := player.moves[player.currentMove]
 	player.currentMove = player.currentMove + 1
-	return move, nil
+	return move, player.setError(move)
+}
+
+func (player *PlayerFake) setError(move int) error {
+	if move > 9 || move < 0 {
+		return errors.New("Out of bounds")
+	}
+	return nil
 }
