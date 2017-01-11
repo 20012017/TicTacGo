6debee96dd3ba88882b7bb87a9847bcd1d6c3b71
20012017 <moll.stephenson@gmail.com>
2017-01-11 10:49:14 +0000

    Adds move method to human player

--- src/ttt/humanplayer_test.go
+++ src/ttt/humanplayer_test.go
@@ -6,11 +6,47 @@ import (
 )
 
 func TestHumanPlayerHasAMark(t *testing.T) {
-	player := HumanPlayer{"X"}
+	player := HumanPlayer{"X", &InputReaderDouble{}}
+
 	assert.Equal(t, "X", player.mark)
 }
 
 func TestHumanPlayerReturnsMark(t *testing.T) {
-	player := HumanPlayer{"X"}
+	player := HumanPlayer{"X", &InputReaderDouble{}}
+
 	assert.Equal(t, "X", player.Mark())
+}
+
+func TestHumanPlayerReadsInput(t *testing.T) {
+	inputReader := &InputReaderDouble{}
+	player := HumanPlayer{"X", inputReader}
+
+	player.move()
+
+	assert.True(t, inputReader.called())
+}
+
+func TestHumanPlayerConvertsStringToIndex(t *testing.T) {
+	player := HumanPlayer{"X", &InputReaderDouble{}}
+
+	move := player.move()
+
+	assert.Equal(t, 0, move)
+}
+
+type InputReaderDouble struct {
+	wasCalled bool
+}
+
+func (reader *InputReaderDouble) setCalled(called bool) {
+	reader.wasCalled = called
+}
+
+func (reader *InputReaderDouble) called() bool {
+	return reader.wasCalled
+}
+
+func (reader *InputReaderDouble) read() string {
+	reader.setCalled(true)
+	return "1\n"
 }
