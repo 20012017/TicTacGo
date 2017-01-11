933624feb08943213479d2cf25f415d89d3cda40
20012017 <moll.stephenson@gmail.com>
2017-01-11 10:49:14 +0000

    Adds move method to human player

--- src/ttt/humanplayer.go
+++ src/ttt/humanplayer.go
@@ -1,9 +1,29 @@
 package ttt
 
+import (
+	"strconv"
+	"strings"
+)
+
 type HumanPlayer struct {
-	mark string
+	mark        string
+	inputReader InputReader
 }
 
 func (player HumanPlayer) Mark() string {
 	return player.mark
+}
+
+func (player HumanPlayer) move() int {
+	move := player.inputReader.read()
+	return player.convertToInt(player.trim(move)) - 1
+}
+
+func (player HumanPlayer) trim(move string) string {
+	return strings.TrimSpace(move)
+}
+
+func (player HumanPlayer) convertToInt(move string) int {
+	validmove, _ := strconv.Atoi(move)
+	return validmove
 }
