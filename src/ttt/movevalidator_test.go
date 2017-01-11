8247ddfd9eb224b8dcfe3657062fde5f667ca9ce
20012017 <moll.stephenson@gmail.com>
2017-01-11 11:32:48 +0000

    Adds move validator

---
+++ src/ttt/movevalidator_test.go
@@ -0,0 +1,60 @@
+package ttt
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDoesNotValidateAWord(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	valid, err := moveValidator.validate("hello\n", NewBoard(9))
+
+	assert.False(t, valid)
+	assert.Equal(t, "Not a number", err.Error())
+}
+
+func TestDoesNotValidateAMoveLargerThanTheBoard(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	valid, err := moveValidator.validate("10\n", NewBoard(9))
+
+	assert.False(t, valid)
+	assert.Equal(t, "Out of bounds", err.Error())
+}
+
+func TestDoesNotValidateAMoveSmallerThanTheBoard(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	valid, err := moveValidator.validate("-1\n", NewBoard(9))
+
+	assert.False(t, valid)
+	assert.Equal(t, "Out of bounds", err.Error())
+}
+
+func TestDoesNotValidateAMoveOnATakenCell(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	markedBoard := NewMarkedBoard([]string{"X", "", "", "", "", "", "", "", ""})
+	valid, err := moveValidator.validate("1\n", markedBoard)
+
+	assert.False(t, valid)
+	assert.Equal(t, "Already taken", err.Error())
+}
+
+func TestValidatesAValidMove(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	valid, err := moveValidator.validate("1\n", NewBoard(9))
+
+	assert.True(t, valid)
+	assert.Nil(t, err)
+}
+
+func TestReturnsValidMove(t *testing.T) {
+	moveValidator := MoveValidator{}
+
+	move := moveValidator.validMove("1\n")
+
+	assert.Equal(t, 0, move)
+}
