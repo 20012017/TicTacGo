89ccee21519592c09e71e5f7371fb1eea8184ff3
20012017 <moll.stephenson@gmail.com>
2017-01-12 12:56:31 +0000

    Adds isOver method to rules and exports Winner

--- src/ttt/core/rules.go
+++ src/ttt/core/rules.go
@@ -4,6 +4,26 @@ import "ttt/core/board"
 
 type Rules struct{}
 
+func (rule Rules) IsOver(tttboard TTTBoard, markOne, markTwo string) bool {
+	return rule.isAWin(tttboard, markOne, markTwo) ||
+		rule.isADraw(tttboard, markOne, markTwo)
+}
+
+func (rule Rules) Winner(tttboard TTTBoard, markOne, markTwo string) string {
+	for _, line := range tttboard.winningPositions() {
+		if rule.winFor(line, markOne) {
+			return markOne
+		} else if rule.winFor(line, markTwo) {
+			return markTwo
+		}
+	}
+	return ""
+}
+
+func (rule Rules) winFor(line board.Line, mark string) bool {
+	return line.All(matches(mark))
+}
+
 func (rule Rules) isAWin(tttboard TTTBoard, markOne, markTwo string) bool {
 	for _, line := range tttboard.winningPositions() {
 		if rule.winFor(line, markOne) || rule.winFor(line, markTwo) {
@@ -18,21 +38,6 @@ func (rule Rules) isADraw(tttboard TTTBoard, markOne, markTwo string) bool {
 		return true
 	}
 	return false
-}
-
-func (rule Rules) winner(tttboard TTTBoard, markOne, markTwo string) string {
-	for _, line := range tttboard.winningPositions() {
-		if rule.winFor(line, markOne) {
-			return markOne
-		} else if rule.winFor(line, markTwo) {
-			return markTwo
-		}
-	}
-	return ""
-}
-
-func (rule Rules) winFor(line board.Line, mark string) bool {
-	return line.All(matches(mark))
 }
 
 func matches(mark string) func(string) bool {
