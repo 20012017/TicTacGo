e9f2694d822f79f0c8e5736e42989aa040b35188
20012017 <moll.stephenson@gmail.com>
2017-01-13 17:10:09 +0000

    Changes menu validator to input validator

---
+++ src/ttt/cli/input/validators/input.go
@@ -0,0 +1,55 @@
+package validators
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
+type Input struct {
+	start, end int
+}
+
+func NewInputValidator(start, end int) Input {
+	return Input{start, end}
+}
+
+func (inputValidator Input) Validate(input string) (choice int, err error) {
+	for _, validation := range inputValidator.validations() {
+		choice, err = validation(inputValidator.formatInput(input))
+		if err != nil {
+			return choice, err
+		}
+	}
+	return choice, nil
+}
+
+func (inputValidator Input) validations() []func(int, error) (int, error) {
+	return []func(int, error) (int, error){
+		inputValidator.validateNumber,
+		inputValidator.validateRange,
+	}
+}
+
+func (inputValidator Input) validateRange(choice int, err error) (int, error) {
+	if choice < inputValidator.start || choice > inputValidator.end {
+		return 0, errors.New("Not in menu")
+	}
+	return choice, nil
+}
+
+func (inputValidator Input) validateNumber(choice int, err error) (int, error) {
+	if err != nil {
+		return 0, errors.New("Not a number")
+	}
+	return choice, nil
+}
+
+func (inputValidator Input) formatInput(input string) (int, error) {
+	formattedString := strings.TrimSpace(input)
+	return inputValidator.convertToInt(formattedString)
+}
+
+func (inputValidator Input) convertToInt(input string) (int, error) {
+	return strconv.Atoi(input)
+}
--- src/ttt/cli/input/validators/input_test.go
+++ src/ttt/cli/input/validators/input_test.go
@@ -6,36 +6,36 @@ import (
 )
 
 func TestDoesNotValidateWord(t *testing.T) {
-	menuValidator := Menu{1, 4}
+	inputValidator := Input{1, 4}
 
-	choice, err := menuValidator.Validate("hello\n")
+	choice, err := inputValidator.Validate("hello\n")
 
 	assert.Equal(t, 0, choice)
 	assert.Equal(t, "Not a number", err.Error())
 }
 
-func TestDoesNotValidateAMoveLargerThanTheMenu(t *testing.T) {
-	menuValidator := Menu{1, 4}
+func TestDoesNotValidateAMoveLargerThanTheInput(t *testing.T) {
+	inputValidator := Input{1, 4}
 
-	choice, err := menuValidator.Validate("10\n")
+	choice, err := inputValidator.Validate("10\n")
 
 	assert.Equal(t, 0, choice)
 	assert.Equal(t, "Not in menu", err.Error())
 }
 
-func TestDoesNotValidateAMoveSmallerThanTheMenu(t *testing.T) {
-	menuValidator := Menu{1, 4}
+func TestDoesNotValidateAMoveSmallerThanTheInput(t *testing.T) {
+	inputValidator := Input{1, 4}
 
-	choice, err := menuValidator.Validate("-1\n")
+	choice, err := inputValidator.Validate("-1\n")
 
 	assert.Equal(t, 0, choice)
 	assert.Equal(t, "Not in menu", err.Error())
 }
 
 func TestValidatesAValidChoice(t *testing.T) {
-	menuValidator := Menu{1, 4}
+	inputValidator := Input{1, 4}
 
-	choice, err := menuValidator.Validate("1\n")
+	choice, err := inputValidator.Validate("1\n")
 
 	assert.Nil(t, err)
 	assert.Equal(t, 1, choice)
