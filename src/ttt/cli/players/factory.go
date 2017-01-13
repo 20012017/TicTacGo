cd459fc7713249fa6e63568791d9746fac03478b
20012017 <moll.stephenson@gmail.com>
2017-01-13 13:48:09 +0000

    Adds player factory

---
+++ src/ttt/cli/players/factory.go
@@ -0,0 +1,25 @@
+package players
+
+import (
+	"ttt/cli/input"
+	"ttt/cli/input/validators"
+	"ttt/core"
+	"ttt/core/player"
+)
+
+type Factory struct {
+	inputReader input.InputReader
+}
+
+func NewPlayerFactory(inputReader input.InputReader) Factory {
+	return Factory{inputReader}
+}
+
+func (factory Factory) player(choice int, mark string) core.Player {
+	if choice == 1 {
+		moveValidator := new(validators.Move)
+		return NewHuman(mark, factory.inputReader, moveValidator)
+	}
+	negamax := player.NewNegamax(new(core.Rules))
+	return player.NewComputer(mark, negamax)
+}
---
+++ src/ttt/cli/players/factory_test.go
@@ -0,0 +1,34 @@
+package players
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestPlayerFactoryReturnsHumanPlayer(t *testing.T) {
+	playerFactory := NewPlayerFactory(new(InputReaderDummy))
+
+	player := playerFactory.player(1, "X")
+	typeOfPlayer := reflect.TypeOf(player)
+
+	assert.Equal(t, "*players.Human", fmt.Sprint(typeOfPlayer))
+	assert.Equal(t, "X", player.Mark())
+}
+
+func TestPlayerFactoryReturnsComputerPlayer(t *testing.T) {
+	playerFactory := NewPlayerFactory(new(InputReaderDummy))
+
+	player := playerFactory.player(2, "X")
+	typeOfPlayer := reflect.TypeOf(player)
+
+	assert.Equal(t, "*player.Computer", fmt.Sprint(typeOfPlayer))
+	assert.Equal(t, "X", player.Mark())
+}
+
+type InputReaderDummy struct{}
+
+func (inputReader InputReaderDummy) Read() string {
+	return ""
+}
