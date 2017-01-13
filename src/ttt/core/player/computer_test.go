2ab2bbb9a0828024cacb26b58abcbd4a9c33ef5a
20012017 <moll.stephenson@gmail.com>
2017-01-13 10:12:20 +0000

    Tests computer player

--- src/ttt/core/player/computer_test.go
+++ src/ttt/core/player/computer_test.go
@@ -1,1 +1,31 @@
 package player
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"ttt/core"
+)
+
+var negamax *Negamax = NewNegamax(new(core.Rules))
+
+func TestHasAMark(t *testing.T) {
+	computer := NewComputer("X", negamax)
+
+	assert.Equal(t, "X", computer.Mark())
+}
+
+func TestMakesAMove(t *testing.T) {
+	computer := NewComputer("X", negamax)
+
+	move, _ := computer.Move(core.NewBoard(9))
+
+	assert.NotNil(t, move)
+}
+
+func TestDoesNotReturnAnError(t *testing.T) {
+	computer := NewComputer("X", negamax)
+
+	_, err := computer.Move(core.NewBoard(9))
+
+	assert.Nil(t, err)
+}
